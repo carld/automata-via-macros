f4392e48cef469f0e1c32e1d593faa8efa84aaf9
Carl <carl.douglas@gmail.com>
2017-01-23 17:50:37 +1300

    Port to Chez scheme, export the automaton macro from an R6RS standard library.
    Note the issue with identifiers in libraries not being equivalent to
    top-level identifiers when they're undefined.

--- automata-via-macros.scm
+++ automata-via-macros.scm
@@ -1,29 +1,44 @@
 ; http://cs.brown.edu/~sk/Publications/Papers/Published/sk-automata-macros/paper.pdf
 ; Figure 5.
 
-(define-module (automata-via-macros)
-   #:export (automaton))
+(library (automata-via-macros)
+         (export automaton : accept ->)
+         (import (rnrs))
+
+; Auxiliary keywords in macros need to be defined because unbound
+; library identifiers aren't equivalent to top-level identifiers
+;
+;     http://gwatt.github.io/csug/use.html#./use:s13
+;     https://github.com/cisco/ChezScheme/issues/41
+
+(define akw
+  (lambda (x) (syntax-violation #f "misplaced auxiliary keyword" x)))
+
+(define : akw)
+(define accept akw)
+(define -> akw)
 
 (define-syntax automaton
   (syntax-rules (:)
-    [(_ init-state 
-       (state : response ...) ...)
+    [(_ init-state
+         (state : response ...) ...)
      (let-syntax
-       ([process-state
-          (syntax-rules (accept ->)
-            [(_ accept)
+          ([process-state
+            (syntax-rules (accept ->)
+              [(_ accept)
                (lambda (stream)
-                 (cond
+                  (cond
                    [(null? stream) #t]
                    [else #f]))]
-            [(_ (label -> target) (... ...))
+              [(_ (label -> target) (... ...))
               (lambda (stream)
-                 (cond
+                  (cond
                    [(null? stream) #f]
                    [else (case (car stream)
                             [(label) (target (cdr stream))]
-                                  (... ...)
+                            (... ...)
                             [else #f])]))])])
-      (letrec ([state (process-state response ...)] ...)
-         init-state))]))
+          (letrec ([state (process-state response ...)] ...)
+            init-state))]))
 
+)
--- test.scm
+++ test.scm
@@ -1,11 +1,12 @@
 
-; add ./ to the guile load path,
+; add ./ to the library load path,
 ; so the automaton module can be loaded from the same
 ; directory as this file is in
-(add-to-load-path (dirname (current-filename)))
+;(add-to-load-path (dirname (current-filename)))
+(library-directories (cons "./" (library-directories)))
 
 ; load the automaton module
-(use-modules (automata-via-macros))
+(import (automata-via-macros))
 
 ; a state machine for parsing car/cdr combinations
 (define m2
@@ -20,9 +21,9 @@
   (syntax-rules()
     ((_ expression expectation)
        (cond
-          ((equal? expression expectation)  
+          ((equal? expression expectation)
                (display "TEST OK" )(newline))
-          (else  (error "Unexpected result " ))))))
+          (else  (error "Unexpected result " expression))))))
 
 (test  (m2 `(c a d r))  #t)
 (test  (m2 `(c d a a d r))  #t)
